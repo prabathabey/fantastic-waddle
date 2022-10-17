b6563a7c694d1d743c065083c3812fe0afd8ac86
Prabath Abeysekara <prabathabeysekara@gmail.com>
2022-10-17 11:10:25 +1100

    Committed the implementation of a sample dockerized flask app.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+import random
+
+app = Flask(__name__)
+
+# list of cat images
+image_url = "/static/images/hello-world.gif"
+
+@app.route("/")
+def index():
+    return render_template("index.html", url=image_url)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
